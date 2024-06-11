fea7f5310c17c216106a27eeff2c07c2c4dead1c
rooddeeo <rooddeeo@gmail.com>
2024-06-11 18:04:55 +0300

    added mobile menu & footer social

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,27 @@
 function toggleMenu() {
-  var menu = document.querySelector(".header-menu-btn");
-  var backdrop = document.querySelector(".backdrop");
+  let heroTitle = document.querySelector(".hero-box-title");
+  let heroParagraph = document.querySelector(".hero-box-paragraph");
+  let heroBtn = document.querySelector(".hero-box-btn");
+  const menu = document.querySelector(".header-mob-menu");
+
+  heroTitle.classList.toggle("hidden");
+  heroParagraph.classList.toggle("hidden");
+  heroBtn.classList.toggle("hidden");
   menu.classList.toggle("open");
-  backdrop.classList.toggle("backdropOn");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const iconBtn = document.querySelector(".header-menu-btn");
+  const iconOpen = document.querySelector(".icon-open");
+  const iconClose = document.querySelector(".icon-close");
+
+  iconBtn.addEventListener("click", () => {
+    if (iconOpen.style.display === "none") {
+      iconOpen.style.display = "inline";
+      iconClose.style.display = "none";
+    } else {
+      iconOpen.style.display = "none";
+      iconClose.style.display = "inline";
+    }
+  });
+});
